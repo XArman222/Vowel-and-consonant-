588af2a393ce501596e1ddcadaf97afa27df0324
XArman222 <xsoulop@gmail.com>
2025-07-23 23:15:16 +0530

    Finding Even or Odd

---
+++ Finding Even or Odd.cpp
@@ -0,0 +1,24 @@
+// Name - Arman Majhi
+// PRN - 24070123022
+// Batch - EnTC - A1 
+
+#include <iostream>
+using namespace std;
+int main() {
+    
+    int num1;
+    cout << "Please enter first number : ";
+    cin >> num1;
+    
+    if(num1 % 2 == 0) cout << "The number "<< num1 << " is even";
+    else cout<< "The number "<< num1 << " is odd";
+    return 0;
+}
+
+//output
+/*
+Please enter first number : 53
+The number 53 is odd
+
+=== Code Execution Successful ===
+/*
